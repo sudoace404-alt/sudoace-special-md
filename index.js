1575a185011cba15398308e5d6224a41a6480dbb
sudoace-special-md <sudoace404@gmail.com>
2025-12-27 18:51:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require('@whiskeysockets/baileys')
 
+const Pino = require('pino')
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState('./nano')
+
+  const sock = makeWASocket({
+    printQRInTerminal: true,
+    logger: Pino({ level: 'silent' }),
+    auth: state
+  })
+
+  sock.ev.on('creds.update', saveCreds)
+
+  sock.ev.on('connection.update', (update) => {
+    const { connection, lastDisconnect } = update
+    if (connection === 'close') {
+      const reason = lastDisconnect?.error?.output?.statusCode
+      if (reason !== DisconnectReason.loggedOut) {
+        startBot()
+      }
+    } else if (connection === 'open') {
+      console.log('✅ BOT SUDAH CONNECT')
+    }
+  })
+}
+
+startBot()
